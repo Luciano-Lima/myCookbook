49ffff4f12344d7d2f34c02c6d1991efccd4b068
Luciano <ccll4ever1@gmail.com>
2019-03-20 18:05:25 +0000

    Created mongo database

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+
 app = Flask(__name__)
 
-#MongoDB config
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+#config
+app.config["MONGO_DBNAME"] = 'cookBook'
+app.config["MONGO_URI"] = 'mongodb+srv://lima:Chiquinho09@myfirstcluster-3jbxl.mongodb.net/cookBook?retryWrites=true'
 
 mongo = PyMongo(app)
 
@@ -19,13 +20,6 @@ def index():
     
     
     
-    
-    
-    
-    
-    
-    
-
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
