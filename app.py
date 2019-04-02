7d94fd0fba1703fcc93fc16161c828ef3578809f
Luciano <ccll4ever1@gmail.com>
2019-04-02 14:07:29 +0000

    Setting up app conection with configuration variables

--- app.py
+++ app.py
@@ -3,21 +3,30 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-
+#App config
 app = Flask(__name__)
-
-#config
-app.config["MONGO_DBNAME"] = 'cookBook'
-app.config["MONGO_URI"] = 'mongodb+srv://lima:Chiquinho09@myfirstcluster-3jbxl.mongodb.net/cookBook?retryWrites=true'
+app.config['MONGO_URI'] = os.environ.get("MONGO_URI")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-
+#Home page fuction
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', recipes=mongo.db.recipes.find())
+    return render_template('index.html')
     
+
+#Recipes fuction
+@app.route('/recipes')
+def recipes():
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
+    
+    
+#Add recipes function
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html')    
     
     
 if __name__ == '__main__':
