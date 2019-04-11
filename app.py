ef18d8aabe9289a62a56cc5d729837603656140f
Luciano <ccll4ever1@gmail.com>
2019-04-11 18:40:56 +0000

    Added registration route

--- app.py
+++ app.py
@@ -1,33 +1,55 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from forms import RegisterForm, LoginForm
 
 #App config
 app = Flask(__name__)
 app.config['MONGO_URI'] = os.environ.get("MONGO_URI")
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
+#wtfform secret key
+app.secret_key = '123456789'
+
 mongo = PyMongo(app)
 
-#Home page fuction
+#Collection
+recipes_coll = mongo.db.recipes
+
+# home
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
     
 
-#Recipes fuction
+#All recipes 
 @app.route('/recipes')
 def recipes():
-    return render_template('recipes.html', recipes=mongo.db.recipes.find())
+    recipes = recipes_coll.find()
+    return render_template('recipes.html', page_title="Easy and Quick Recipes",  recipes=recipes)
     
     
-#Add recipes function
+#Add recipes 
 @app.route('/add_recipe')
 def add_recipe():
     return render_template('addrecipe.html')    
     
+
+#User registration
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form =  registerForm()
+    #checking for data validation on Post
+    if form.validate_on_submit():
+         flash('Hi {}, your account has been created!'.format({form.username.data}), 'success')
+         return redirect(url_for('index'))
+    return render_template('register.html', page_title="New User Register", form=form)    
+    
+
+
+
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
