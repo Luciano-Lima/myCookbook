dabe8c36e10bf1bbf7f1c391591841c6d4fdaaac
Luciano <ccll4ever1@gmail.com>
2019-04-11 17:25:12 +0000

    Added user login form

--- forms.py
+++ forms.py
@@ -2,9 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
-class RegistrationForm(FlaskForm):
+
+#User registration form
+class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(),Length(min=4, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=15)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message= 'Password do not match')])
     submit = SubmitField('Sign UP')
+    
+    
+#User login form 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired])
+    remember = StringField('Remember Me')
+    submit = SubmitField('Login')
