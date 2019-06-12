da30540e5f1d783f6218500af1ef6af94bc2e2cf
Luciano <ccll4ever1@gmail.com>
2019-06-12 21:15:02 +0000

    Added jquery to togle the allergens button

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,12 +1,30 @@
-$(document).ready(function () {
+//Add aditional ingredient row
+$(document).ready(function(){
+	$('#addrow').click(function() {
+		$('<input class="form-control is-invalid" id="ingredient" name="ingredient" required type="text" value=""><br>').insertBefore('#addrow');	
+		
+	});	
+	//Add aditional preparations step row
+	$('#addrowSteps').click(function() {
+		$('<input class="form-control is-invalid" id="step" name="step" required type="text" value=""><br>').insertBefore('#addrowSteps');
 
-	/* Login Slider */
-	$("#login-button").on("click", function() {
-		$("#login").toggleClass("login-clicked");
-		$("#login-button").toggleClass("login-clicked");
-		$("#login").animate( {
-			width: 'toggle'
-		});
+	});	
+	
+	// Hide allergens menu
+	$('#allergens li').hide() 
+	
+	
+	// Show allergens on click function 
+	$('.fa-angle-down').click(function() {
+	    $('#allergens li').slideDown(600)
+	    	
 	});
+	
+		// // $('#removerow').click(function() {
+	// // 	$('#ingredients input:last').remove();
+	// // });
+	    
+});	
+	
+	
 
-});
