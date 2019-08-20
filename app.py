83d40d4e94d7fdb09824d5262af414180b345134
Luciano <ccll4ever1@gmail.com>
2019-08-20 17:02:10 +0000

    Added validator lenght to addrecipe form

--- app.py
+++ app.py
@@ -217,9 +217,10 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
     
+    
+    
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-    
